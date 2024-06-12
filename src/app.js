a1f52f9503da7347ac4578cb59d9da310c8bf487
Pedro Hoffmann <pedrohoff87@gmail.com>
2024-06-12 17:56:22 -0300

    Api já finalizada

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv').config();
+const taskRouter = require('./routes/taskRouter');
+const cors =  require('cors');
 const app = express();
+
 app.set('port', process.env.port || 3000);
+app.use(express.json());
+app.use(cors());
+app.use('/api', taskRouter);
 
 module.exports = app;
